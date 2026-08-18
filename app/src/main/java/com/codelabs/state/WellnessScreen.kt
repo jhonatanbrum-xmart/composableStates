6a9e227eea3d3461482d811d81deb1e3aaef17e0
jhonatanbrum-xmart <jhonatan.brum@xmartlabs.com>
2026-08-18 17:41:52 -0300

    Separated composables and classes

---
+++ app/src/main/java/com/codelabs/state/WellnessScreen.kt
@@ -0,0 +1,32 @@
+package com.codelabs.state
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.codelabs.state.ui.theme.BasicStateCodelabTheme
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier, wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(list = wellnessViewModel.tasks, onCheckedTask = { task, checked ->
+            wellnessViewModel.changeTaskChecked(task, checked)
+        }, onCloseTask = { task ->
+            wellnessViewModel.remove(task)
+        })
+    }
+}
+
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasicStateCodelabTheme {
+        WellnessScreen()
+    }
+}
---
+++ app/src/main/java/com/codelabs/state/WellnessTasksList.kt
@@ -0,0 +1,60 @@
+package com.codelabs.state
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f).padding(start = 16.dp), text = taskName
+        )
+        Checkbox(
+            checked = checked, onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+@Composable
+fun WellnessTasksList(
+    list: List<WellnessTask>,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = list, key = { task -> task.id }) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) })
+        }
+    }
+}
